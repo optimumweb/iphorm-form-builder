e511b9c6b9cb9cb84f29120e4faa335496232e53
Jonathan Roy <jroy@optimumweb.ca>
2013-02-15 21:25:22 -0500

    Fix

--- includes/common.php
+++ includes/common.php
@@ -826,7 +826,7 @@ function iphorm_process_form()
 
                 $podio_fields = array();
                 foreach ($elements as $element) {
-                    wpbp_error_log(var_export($element, true));
+                    wpbp_error_log(var_export($element->getPodioId(), true));
                     if ($element->getPodioId()) {
                         $podio_fields[$element->getPodioId()] = $element->getValue();
                     }
